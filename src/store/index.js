67bca051d9059cf5ba5ee5be98f962c8175ea6b6
lana-k <skochetova89@gmail.com>
2020-10-09 22:16:26 +0200

    fix path to worker

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
     schema: null,
     dbFile: null,
     dbName: null,
-    worker: new Worker('/js/worker.sql-wasm.js'),
+    worker: new Worker('js/worker.sql-wasm.js'),
     tabs: [],
     currentTab: null,
     currentTabId: null,
