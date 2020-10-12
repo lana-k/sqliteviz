dbc2e3d0f319608f1fa22c724a59c5064a5c4242
lana-k <skochetova89@gmail.com>
2020-10-12 22:48:10 +0200

    add VModal

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { VuePlugin } from 'vuera'
+import VModal from 'vue-js-modal'
 
 import '@/assets/styles/variables.css'
 import '@/assets/styles/buttons.css'
 import '@/assets/styles/textFields.css'
 import '@/assets/styles/tables.css'
+import '@/assets/styles/dialogs.css'
 
 Vue.use(VuePlugin)
+Vue.use(VModal)
 
 Vue.config.productionTip = false
 
