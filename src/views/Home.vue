3b43fcddb4b2a78ef288ec9ff3d0342c72af92db
lana-k <skochetova89@gmail.com>
2020-11-25 21:41:38 +0100

    remove unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,33 +13,7 @@ import dbUpload from '@/components/DbUpload'
 
 export default {
   name: 'Home',
-  components: { dbUpload },
-  methods: {
-    loadDb () {
-      this.$db.loadDb(this.$refs.file.files[0])
-    },
-    dragover (event) {
-      event.preventDefault()
-      // TODO: Add some visual stuff to show the user can drop its files
-      if (!event.currentTarget.classList.contains('bg-green-300')) {
-        event.currentTarget.classList.remove('bg-gray-100')
-        event.currentTarget.classList.add('bg-green-300')
-      }
-    },
-    dragleave (event) {
-      // Clean up
-      event.currentTarget.classList.add('bg-gray-100')
-      event.currentTarget.classList.remove('bg-green-300')
-    },
-    drop (event) {
-      event.preventDefault()
-      this.$refs.file.files = event.dataTransfer.files
-      this.loadDb()
-      // Clean up
-      event.currentTarget.classList.add('bg-gray-100')
-      event.currentTarget.classList.remove('bg-green-300')
-    }
-  }
+  components: { dbUpload }
 }
 </script>
 
