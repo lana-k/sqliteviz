c2a155c56150bee8553fca2cd7c9887a5b74ba97
lana-k <skochetova89@gmail.com>
2020-12-16 22:38:05 +0100

    License file is missing in the build #21

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ module.exports = {
         // This wasm file will be fetched dynamically when we initialize sql.js
         // It is important that we do not change its name, and that it is in the same folder as the js
         { from: 'node_modules/sql.js/dist/sql-wasm.wasm', to: 'js/' },
-        { from: 'node_modules/sql.js/dist/worker.sql-wasm.js', to: 'js/' }
+        { from: 'node_modules/sql.js/dist/worker.sql-wasm.js', to: 'js/' },
+        { from: 'LICENSE', to: './' }
       ])
     ]
   }
