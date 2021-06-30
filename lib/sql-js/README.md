f617dcb3fcecabe0f24939d04987e079d55d5fcf
saaj <id@saaj.me>
2021-06-30 23:35:28 +0200

    Rewrite Makefile as a couple of comprehensible Python scripts

--- lib/sql-js/README.md
+++ lib/sql-js/README.md
@@ -1,21 +1,26 @@
 # SQLite WebAssembly build
 
-This directory contains Docker-based build script, `build.sh`, that builds
-custom version of [sql.js][1]. It uses more recent version of SQLite itself.
+This directory contains Docker-based build script, `make.sh`, that builds
+a custom version of [sql.js][1]. It allows sqliteviz to have more recent
+version of SQLite build with a number of useful extensions.
+
+`Makefile` from [sql.js][1] is rewritten as more comprehensible `configure.py`
+ and `build.py` Python scripts that run in `emscripten/emsdk` Docker container.
+
+## Extension
 
 SQLite [amalgamation][2] extensions included:
 
 1. [FTS5][4] -- virtual table module that provides full-text search
    functionality
 
-TODO: SQLite [miscellaneous extensions][3] included:
+SQLite [miscellaneous extensions][3] included:
 
 1. `generate_series` table-valued [series function][6] ([series.c][7])
 2. `transitive_closure` virtual table ([closure.c][8])
-3. `compress` and `uncompress` functions using ZLIB ([compress.c][9])
-4. `uuid`, `uuid_str` and `uuid_blob` RFC-4122 UUID functions ([uuid.c][10])
-5. `regexp` function ([regexp.c][11])
-6. `eval` function which runs SQL statements recursively ([eval.c][12])
+3. `uuid`, `uuid_str` and `uuid_blob` RFC-4122 UUID functions ([uuid.c][9])
+4. `regexp` function ([regexp.c][10])
+5. `eval` function which runs SQL statements recursively ([eval.c][11])
 
 SQLite 3rd party extensions included:
 
@@ -24,7 +29,35 @@ SQLite 3rd party extensions included:
 To ease the step to have working clone locally, the build is committed into
 the repository.
 
-`Makefile.sql-js` is a modified version of the `Makefile` from [sql.js][1].
+## Build method
+
+Basically it's extended amalgamation and `SQLITE_EXTRA_INIT` concisely
+described in [this message from SQLite Forum][12]:
+
+> Simply append it to the end of the amalgamation file. The real problem is
+> how you get the init function called. The easiest way (to me at any rate) is
+> to append a function (after the extensions you want to add are all appended)
+> that adds the init function for each extension to the auto extension list
+> for new connections, and set the pre-processor symbol SQLITE_EXTRA_INIT to
+> the name of this function. [...]
+>
+> An example `SQLITE_EXTRA_INIT` function looks like this:
+>
+> ```
+> int core_init(const char* dummy)
+> {
+>    int nErr = 0;
+>
+>    nErr += sqlite3_auto_extension((void*)sqlite3_autobusy_init);
+>    nErr += sqlite3_auto_extension((void*)sqlite3_ipaddress_init);
+>
+>    return nErr ? SQLITE_ERROR : SQLITE_OK;
+> }
+> ```
+>
+> so you would then define `SQLITE_EXTRA_INIT=core_init` when compiling the
+> amalgamation code and the extensions would thereafter be automatically
+> initialized on each connection.
 
 [1]:  https://github.com/sql-js/sql.js
 [2]:  https://sqlite.org/amalgamation.html
@@ -34,7 +67,7 @@ the repository.
 [6]:  https://sqlite.org/series.html
 [7]:  https://sqlite.org/src/file/ext/misc/series.c
 [8]:  https://sqlite.org/src/file/ext/misc/closure.c
-[9]:  https://sqlite.org/src/file/ext/misc/compress.c
-[10]: https://sqlite.org/src/file/ext/misc/uuid.c
-[11]: https://sqlite.org/src/file/ext/misc/regexp.c
-[12]: https://sqlite.org/src/file/ext/misc/eval.c
+[9]:  https://sqlite.org/src/file/ext/misc/uuid.c
+[10]: https://sqlite.org/src/file/ext/misc/regexp.c
+[11]: https://sqlite.org/src/file/ext/misc/eval.c
+[12]: https://sqlite.org/forum/forumpost/6ad7d4f4bebe5e06?raw
---
+++ lib/sql-js/configure.py
@@ -0,0 +1,106 @@
+import logging
+import shutil
+import subprocess
+import sys
+import zipfile
+from io import BytesIO
+from pathlib import Path
+from urllib import request
+
+
+amalgamation_url = 'https://sqlite.org/2021/sqlite-amalgamation-3360000.zip'
+
+# Extension-functions
+# ===================
+# It breaks amalgamation if appended as other extension because it redefines
+# several functions, so build it separately. Note that sql.js registers these
+# extension functions by calling ``registerExtensionFunctions`` itself.
+contrib_functions_url = 'https://sqlite.org/contrib/download/extension-functions.c?get=25'
+
+extension_urls = (
+    # Miscellaneous extensions
+    # ========================
+    ('https://sqlite.org/src/raw/c6bd5d24?at=series.c', 'sqlite3_series_init'),
+    ('https://sqlite.org/src/raw/dbfd8543?at=closure.c', 'sqlite3_closure_init'),
+    ('https://sqlite.org/src/raw/5bb2264c?at=uuid.c', 'sqlite3_uuid_init'),
+    ('https://sqlite.org/src/raw/5853b0e5?at=regexp.c', 'sqlite3_regexp_init'),
+    ('https://sqlite.org/src/raw/04bc9aad?at=eval.c', 'sqlite3_eval_init'),
+    # Third-party extension
+    # =====================
+    ('https://github.com/jakethaw/pivot_vtab/raw/08ab0797/pivot_vtab.c', 'sqlite3_pivotvtab_init'),
+)
+
+sqljs_url = 'https://github.com/sql-js/sql.js/archive/refs/tags/v1.5.0.zip'
+
+
+def _generate_extra_init_c_function(init_function_names):
+    auto_ext_calls = '\n'.join([
+        'nErr += sqlite3_auto_extension((void*){});'.format(init_fn)
+        for init_fn in init_function_names
+    ])
+    return '''
+        int extra_init(const char* dummy)
+        {
+            int nErr = 0;
+            %s
+            return nErr ? SQLITE_ERROR : SQLITE_OK;
+        }
+    ''' % auto_ext_calls
+
+
+def _get_amalgamation(tgt: Path):
+    logging.info('Downloading and extracting SQLite amalgamation %s', amalgamation_url)
+    archive = zipfile.ZipFile(BytesIO(request.urlopen(amalgamation_url).read()))
+    archive_root_dir = zipfile.Path(archive, archive.namelist()[0])
+    for zpath in archive_root_dir.iterdir():
+        with zpath.open() as fr, (tgt / zpath.name).open('wb') as fw:
+            shutil.copyfileobj(fr, fw)
+
+
+def _get_contrib_functions(tgt: Path):
+    request.urlretrieve(contrib_functions_url, tgt / 'extension-functions.c')
+
+
+def _get_extensions(tgt: Path):
+    init_functions = []
+    sqlite3_c = tgt / 'sqlite3.c'
+    with sqlite3_c.open('ab') as f:
+        for url, init_fn in extension_urls:
+            logging.info('Downloading and appending to amalgamation %s', url)
+            with request.urlopen(url) as resp:
+                shutil.copyfileobj(resp, f)
+            init_functions.append(init_fn)
+
+        logging.info('Appending SQLITE_EXTRA_INIT to amalgamation')
+        f.write(_generate_extra_init_c_function(init_functions).encode())
+
+
+def _get_sqljs(tgt: Path):
+    logging.info('Downloading and extracting sql.js %s', sqljs_url)
+    archive = zipfile.ZipFile(BytesIO(request.urlopen(sqljs_url).read()))
+    archive_root_dir = zipfile.Path(archive, archive.namelist()[0])
+    (tgt / 'sqljs').mkdir()
+    for zpath in (archive_root_dir / 'src').iterdir():
+        with zpath.open() as fr, (tgt / 'sqljs' / zpath.name).open('wb') as fw:
+            shutil.copyfileobj(fr, fw)
+
+
+def configure(tgt: Path):
+    _get_amalgamation(tgt)
+    _get_contrib_functions(tgt)
+    _get_extensions(tgt)
+    _get_sqljs(tgt)
+
+    subprocess.check_call(['emcc', '--version'])
+
+
+if __name__ == '__main__':
+    if sys.version_info < (3, 8):
+        print('Python 3.8 or higher is expected', file=sys.stderr)
+        sys.exit(1)
+
+    logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)s %(name)s %(message)s')
+
+    src = Path('src')
+    src.mkdir()
+    configure(src)
