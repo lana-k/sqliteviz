3a6628cab93ab3735bb0248974dbc528ca953bbb
saaj <id@saaj.me>
2021-07-03 16:43:43 +0200

    Pre-built custom sql.js for sqliteviz (#62)
    * Proof-of-concept pre-built custom sql.js
    
    * Rewrite Makefile as a couple of comprehensible Python scripts
    
    * Add link to a blog post about transitive closure in SQLite
    
    * Remove eval extension -- no much point as it only returns a string
    
    * Consistently use existing Path objects
    
    * Add basic tests scalar functions from extension-functions.c
    
    * Test presence of functions from the rest of built extensions
    
    * Use the same sqlite.com domain
    
    * Add a couple SQLite compile flags that may make it a bit faster
    
    * Add regexpi function test
    
    * Add node about regexpi and REGEXP operator
    
    * Workaround first build failure, rebuild lock file and minor fixes

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   publicPath: '',
+  // Workaround for https://github.com/vuejs/vue-cli/issues/5399 as described
+  // in https://stackoverflow.com/a/63185174
+  lintOnSave: process.env.NODE_ENV === 'development',
   configureWebpack: {
     plugins: [
       new CopyPlugin([
