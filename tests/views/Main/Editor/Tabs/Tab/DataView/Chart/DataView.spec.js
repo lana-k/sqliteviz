c9a6debe63fecc59f2a12c53877e254e0b6644e5
lana-k <skochetova89@gmail.com>
2021-07-25 15:29:44 +0200

    add test for DataView #5

---
+++ tests/views/Main/Editor/Tabs/Tab/DataView/Chart/DataView.spec.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai'
+import { mount, createWrapper } from '@vue/test-utils'
+import DataView from '@/views/Main/Editor/Tabs/Tab/DataView'
+
+describe('DataView.vue', () => {
+  it('emits update on mode changing', async () => {
+    const wrapper = mount(DataView)
+
+    const pivotBtn = createWrapper(wrapper.findComponent({ name: 'pivotIcon'}).vm.$parent)
+    await pivotBtn.trigger('click')
+
+    expect(wrapper.emitted('update')).to.have.lengthOf(1)
+  })
+})
