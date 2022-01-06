f9561b8678f9927e2ab8237259d4017b1bc1c153
lana-k <skochetova89@gmail.com>
2022-01-06 21:29:46 +0100

    clear branch

--- lib/sql-js/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-docker build -t sqliteviz/sqljs .
-
-rm -r dist || true
-
-CONTAINER=$(docker create sqliteviz/sqljs)
-docker cp $CONTAINER:/tmp/build/dist .
-docker rm $CONTAINER
--- src/store/state.js
+++
@@ -1,8 +0,0 @@
-export default {
-  tabs: [],
-  currentTab: null,
-  currentTabId: null,
-  untitledLastIndex: 0,
-  predefinedInquiries: [],
-  db: null
-}
