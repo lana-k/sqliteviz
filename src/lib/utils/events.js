48e311bff874990bcd7afa9affd3d6eed70bc30b
lana-k <skochetova89@gmail.com>
2022-06-24 22:58:43 +0200

    minor change

--- src/lib/utils/events.js
+++ src/lib/utils/events.js
@@ -1,8 +1,5 @@
 export function send (payload) {
   console.log(payload)
-  if (!window.sendEvent) {
-    return
-  }
 
   const event = new CustomEvent('sqliteviz-app-event', {
     detail: payload
